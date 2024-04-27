5a9227c3002f81e5897dbbea285199dc21baea6e
Yu-Xi Lim <limyuxi@gmail.com>
2024-04-27 19:23:58 +0800

    Skip, don't exit

--- sgpt-zsh.plugin.zsh
+++ sgpt-zsh.plugin.zsh
@@ -2,18 +2,17 @@
 
 if ! (( $+commands[sgpt] )); then
     echo 'sgpt command not found: please install via https://github.com/TheR1D/shell_gpt'
-    exit
+else
+    # Shell-GPT integration ZSH v0.2
+    _sgpt_zsh() {
+    if [[ -n "$BUFFER" ]]; then
+        _sgpt_prev_cmd=$BUFFER
+        BUFFER+="⌛"
+        zle -I && zle redisplay
+        BUFFER=$(sgpt --shell <<< "$_sgpt_prev_cmd" --no-interaction)
+        zle end-of-line
+    fi
+    }
+    zle -N _sgpt_zsh
+    bindkey '^l' _sgpt_zsh
 fi
-
-# Shell-GPT integration ZSH v0.2
-_sgpt_zsh() {
-if [[ -n "$BUFFER" ]]; then
-    _sgpt_prev_cmd=$BUFFER
-    BUFFER+="⌛"
-    zle -I && zle redisplay
-    BUFFER=$(sgpt --shell <<< "$_sgpt_prev_cmd" --no-interaction)
-    zle end-of-line
-fi
-}
-zle -N _sgpt_zsh
-bindkey '^l' _sgpt_zsh
